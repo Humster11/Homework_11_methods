47a66eb7f44b749975c95f178a2b4289fa3fe6c3
daria.lapina <ms.humster11@gmail.com>
2024-11-18 21:01:46 +0300

    Исправленное ДЗ

--- src/Task3.java
+++ src/Task3.java
@@ -2,16 +2,16 @@ public class Task3 {
 
     public static int countDayDelivery(int deliveryDistance) {
         int deliveryTime = 0;
-        if (deliveryDistance < 100) {
-            if (deliveryDistance <= 20) {
-                deliveryTime = deliveryTime + 1;
-            } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
-                deliveryTime = deliveryTime + 2;
-            } else if (deliveryDistance > 60) {
-                deliveryTime = deliveryTime + 3;
-            }
+
+         if (deliveryDistance <= 20) {
+            deliveryTime = deliveryTime + 1;
+        } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
+           deliveryTime = deliveryTime + 2;
+        } else if (deliveryDistance > 60 && deliveryDistance<100) {
+            deliveryTime = deliveryTime + 3;
         }
         return deliveryTime;
+
     }
 
     public static void main(String[] args) {
